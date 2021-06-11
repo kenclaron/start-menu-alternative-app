947abd4cc3c0fbe325faa3f2b2572bd0873e779f
kenclaron <kenclaron@gmail.com>
2021-06-11 19:38:11 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StartMenuAlternativeApp
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags,
+           UIntPtr dwExtraInfo);
+        const byte KEY_CTRL = 0x11;
+        const byte KEY_ESC = 0x1B;
+        const uint KEYEVENTF_KEYUP = 0x02;
+
+        public static void OpenStartMenu()
+        {
+            keybd_event(KEY_CTRL, 0, 0, UIntPtr.Zero);
+            keybd_event(KEY_ESC, 0, 0, UIntPtr.Zero);
+
+            keybd_event(KEY_CTRL, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+            keybd_event(KEY_ESC, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+
+            return;
+        }
+
+        static void Main()
+        {
+            OpenStartMenu();
+
+            return;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+﻿Start Menu - Alternative App
+============================
+
+About
+-----
+This application can be pinned to the taskbar and open the Windows Start Menu.
+To get a better quality of use, I recommend using it together with [CenterTaskbar by mdhiggins](https://github.com/mdhiggins/CenterTaskbar)
+
+Usage
+--------------------------
+1. Press the Right Mouse Button on the application "Start Menu - Alternative App.exe";
+2. Click "Pin to taskbar".
+
+Requirements
+------------
+* Windows 10 1903 (build 18362) or newer;
+* [.NET 5.0](https://dotnet.microsoft.com/download/dotnet/5.0) or newer.
+
+License
+-------
+Licensed under the MIT license.
+> Microsoft Windows, Logo are trademarks of the Microsoft group of companies
